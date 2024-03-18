9f3c9fb2d3d33ee02f234248532b440503b050c4
Francisco Ramirez Vaquero <francisco.ramirez@jcarrion.es>
2024-03-18 13:24:29 +0100

    commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = [];
+
+  onDeleteCharacter(id?: string): void {
+    // console.log({ index });
+    if (!id) return;
+    this.onDelete.emit(id);
+  }
+}
